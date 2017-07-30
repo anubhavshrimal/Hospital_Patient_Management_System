39c59f0eee0848bb382cd3e3a56fbac9930d7ff6
anubhavshrimal <anubhavshrimal@gmail.com>
2017-07-30 14:26:19 +0530

    dashboard ngIf

--- public/src/app/components/login/login.component.ts
+++ public/src/app/components/login/login.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import{GetService} from "../../services/get.service"
-import {AuthenticateUserService} from "../../services/authenticate-user.service"
-import {PatientLogin} from './login'
+import{GetService} from "../../services/get.service";
+import {PatientLogin} from './login';
 import {PostService} from "../../services/post.service";
-import {Router} from "@angular/router"
+import {Router} from "@angular/router";
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  providers:[PostService,AuthenticateUserService]
+  providers:[PostService]
 })
 export class LoginComponent implements OnInit {
   found:boolean;
   userType:any;
   userlogin:PatientLogin;
-  constructor(private postservice:PostService,private auth:AuthenticateUserService,private router:Router) {
+  constructor(private postservice:PostService, private router:Router) {
     this.userlogin=new PatientLogin();
     this.found=true;
       }
@@ -33,8 +33,7 @@ export class LoginComponent implements OnInit {
       var utype=data1.userType;
       var token=data1.token;
       console.log(utype)
-      this.auth.changeUser(utype);
-      localStorage.setItem('userData', 'data1');
+      localStorage.setItem('userData', JSON.stringify(data1));
       this.router.navigate([`/home`]);
     }
   }
