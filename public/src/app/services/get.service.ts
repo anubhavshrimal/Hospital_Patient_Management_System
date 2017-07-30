b40538983951e7f32236780d23261dce3b8eab61
deepak lahoty <deepaklahoty5d@gmail.com>
2017-07-30 13:39:59 +0530

    done

--- public/src/app/services/get.service.ts
+++ public/src/app/services/get.service.ts
@@ -7,6 +7,10 @@ export class GetService {
   constructor(private http:Http) {
 
   }
+  getSymptoms(){
+    console.log('here');
+    return this.http.get('http://192.168.1.102:3000/dataStore/medicalConditions');
+  }
 
 
 
